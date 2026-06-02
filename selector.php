14c0f1486ff9a51ff2fd57a5038344198c328e31
Quinten-7Lego <quinten.brouwer@ichthuslyceum.nl>
2026-06-02 08:36:59 +0000

    grid

---
+++ selector.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+<title>Paginatemplate</title>
+<meta charset="utf-8">
+<meta name="author" content="Quinten">
+<meta name="keywords" content="template,HTML,metatags">
+<meta name="description" content="Dit is de HTML5 template voor iedere pagina">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body> 
+
+<header>
+    <img>  
+    <h1> Telstar </h1>
+    <address> 
+    </address>
+</header>
+<nav> 
+    <a href=" " id= "super">link</h1>
+    <a href=" " class= "belangerijk">linkje</a>
+    <a href=" ">grote link</a>
+</nav>
+<main> 
+    <p> Standen </p>
+    <p> Ons team</p>
+    <p> Tickets </p>
+</main>
+</body>
+</html>
+<div class="search">
+        <input type="text" placeholder="zoeken">
+    </div>
