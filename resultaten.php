59fada3266b1b4128c92770e1386fac807e015e7
moayadkanaan <moayad.kanaan@ichthuslyceum.nl>
2026-06-09 07:51:43 +0000

    commitnaarquinten

--- resultaten.php
+++ resultaten.php
@@ -1,11 +1,52 @@
-<?php include('db/connect.php'); ?><!DOCTYPE html>
+<?php
+$db = new PDO('sqlite:eredivisie.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$clubNaam = 'Telstar';
+$stmt = $db->prepare("SELECT clubid FROM clubs WHERE naam = ?");
+$stmt->execute([$clubNaam]);
+$clubId = $stmt->fetchColumn();
+
+$sql = "
+SELECT w.*, thuis.naam AS thuisnaam, uit.naam AS uitnaam
+FROM wedstrijden w
+JOIN clubs thuis ON thuis.clubid = w.thuisclub
+JOIN clubs uit ON uit.clubid = w.uitclub
+WHERE w.thuisclub = :clubid OR w.uitclub = :clubid
+ORDER BY w.yyyy DESC, w.mm DESC, w.dd DESC
+LIMIT 10
+";
+$stmt = $db->prepare($sql);
+$stmt->execute(['clubid' => $clubId]);
+$wedstrijden = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+function getLogo($club) {
+    $logos = [
+        'Telstar'         => 'img/logo.png',
+        'Ajax'            => 'https://upload.wikimedia.org/wikipedia/sco/thumb/7/79/Ajax_Amsterdam.svg/3840px-Ajax_Amsterdam.svg.png',
+        'PSV'             => 'https://upload.wikimedia.org/wikipedia/sco/thumb/0/05/PSV_Eindhoven.svg/1280px-PSV_Eindhoven.svg.png',
+        'Feyenoord'       => 'https://upload.wikimedia.org/wikipedia/sco/thumb/e/e3/Feyenoord_logo.svg/1280px-Feyenoord_logo.svg.png',
+        'AZ'              => 'https://images.seeklogo.com/logo-png/1/2/az-logo-png_seeklogo-14935.png',
+        "AZ `67"          => 'https://images.seeklogo.com/logo-png/27/1/az-67-alkmaar-logo-png_seeklogo-275285.png',
+        'FC Utrecht'      => 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Logo_FC_Utrecht.svg/1920px-Logo_FC_Utrecht.svg.png',
+        'FC Twente'       => 'https://cdn.worldvectorlogo.com/logos/fc-twente.svg',
+        "FC Twente `65"   => 'https://images.seeklogo.com/logo-png/5/2/fc-twente-65-logo-png_seeklogo-53071.png',
+        'Roda JC'         => 'https://images.seeklogo.com/logo-png/53/2/roda-jc-kerkrade-logo-png_seeklogo-536640.png',
+        'NAC'             => 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Logo_NAC_Breda.svg/960px-Logo_NAC_Breda.svg.png',
+        'Vitesse'         => 'https://cdn.worldvectorlogo.com/logos/vitesse-2.svg',
+        'Go Ahead Eagles' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Go_Ahead_Eagles_logo.svg/330px-Go_Ahead_Eagles_logo.svg.png',
+        'FC Volendam'     => 'https://upload.wikimedia.org/wikipedia/en/thumb/0/0e/FC_Volendam_logo.svg/1280px-FC_Volendam_logo.svg.png',
+    ];
+    return $logos[$club] ?? 'https://cdn-icons-png.flaticon.com/512/53/53283.png';
+}
+?><!DOCTYPE html>
 <html lang="nl">
     <head>
-        <title>Mannen - Telstar</title>
+        <title>Resultaten - Telstar</title>
         <meta charset="utf-8">
         <meta name="author" content="Telstar Webteam">
-        <meta name="keywords" content="telstar, selectie, spelers, mannen, eredivisie">
-        <meta name="description" content="De officiële selectie van Telstar seizoen 2025/2026">
+        <meta name="keywords" content="telstar, resultaten, uitslagen, wedstrijden">
+        <meta name="description" content="Laatste wedstrijden van Telstar">
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
@@ -32,8 +73,8 @@
             <nav>
                 <ul>
                     <li><a href="index.php">&#127968; Home</a></li>
-                    <li><a href="mannen.php" class="actief">&#128085; Mannen</a></li>
-                    <li><a href="resultaten.php">&#9917; Resultaten</a></li>
+                    <li><a href="mannen.php">&#128085; Mannen</a></li>
+                    <li><a href="resultaten.php" class="actief">&#9917; Resultaten</a></li>
                     <li><a href="webshop.php">&#128722; Webshop</a></li>
                     <li><a href="declub.php">&#127942; De Club</a></li>
                 </ul>
@@ -51,68 +92,46 @@
         <main>
             <section class="pagina-header">
                 <div>
-                    <h1>Mannen</h1>
-                    <p>Officiële selectie seizoen 2025/2026 · Eredivisie</p>
+                    <h1>Resultaten</h1>
+                    <p>Laatste 10 wedstrijden van Telstar</p>
                 </div>
             </section>
 
-            <?php
-            $tm = 'https://img.a.transfermarkt.technology/portrait/medium/';
-
-            $selectie = [
-                'Keepers' => [
-                    ['nr'=>1,  'naam'=>'Ronald Koeman Jr.',          'nat'=>'🇳🇱', 'foto'=>$tm.'271549-1768294767.JPG?lm=1'],
-                    ['nr'=>13, 'naam'=>'Tyrick Bodak',               'nat'=>'🇳🇱', 'foto'=>$tm.'663961-1768295206.JPG?lm=1'],
-                    ['nr'=>20, 'naam'=>'Daan Reiziger',              'nat'=>'🇳🇱', 'foto'=>$tm.'412946-1768295385.JPG?lm=1'],
-                ],
-                'Verdedigers' => [
-                    ['nr'=>2,  'naam'=>'Jeff Hardeveld',             'nat'=>'🇳🇱', 'foto'=>$tm.'412946-1768295385.JPG?lm=1'],
-                    ['nr'=>3,  'naam'=>'Gerald Alders',              'nat'=>'🇳🇦', 'foto'=>$tm.'891854-1738694528.jpg?lm=1'],
-                    ['nr'=>4,  'naam'=>'Guus Offerhaus',             'nat'=>'🇳🇱', 'foto'=>$tm.'590777-1768294845.JPG?lm=1'],
-                    ['nr'=>5,  'naam'=>'Nigel Ogidi Nwankwo',        'nat'=>'🇳🇱', 'foto'=>$tm.'399350-1768294928.JPG?lm=1'],
-                    ['nr'=>6,  'naam'=>'Danny Bakker',               'nat'=>'🇳🇱', 'foto'=>$tm.'250440-1768294965.JPG?lm=1', 'captain'=>true],
-                    ['nr'=>14, 'naam'=>'Neville Ogidi Nwankwo',      'nat'=>'🇳🇱', 'foto'=>$tm.'951398-1768294891.JPG?lm=1'],
-                    ['nr'=>15, 'naam'=>'Adil Lechkar',               'nat'=>'🇳🇱', 'foto'=>$tm.'1006923-1768295243.JPG?lm=1'],
-                    ['nr'=>21, 'naam'=>'Devon Koswal',               'nat'=>'🇸🇷', 'foto'=>$tm.'748604-1768295424.JPG?lm=1'],
-                    ['nr'=>29, 'naam'=>'Dion Malone',                'nat'=>'🇸🇷', 'foto'=>$tm.'84405-1768295568.jpg?lm=1'],
-                ],
-                'Middenvelders' => [
-                    ['nr'=>8,  'naam'=>'Tyrone Owusu',               'nat'=>'🇳🇱', 'foto'=>$tm.'461644-1768295043.JPG?lm=1'],
-                    ['nr'=>16, 'naam'=>'Dylan Mertens',              'nat'=>'🇳🇱', 'foto'=>$tm.'322616-1768295277.jpg?lm=1'],
-                    ['nr'=>17, 'naam'=>'Nils Rossen',                'nat'=>'🇳🇱', 'foto'=>$tm.'694039-1768295313.JPG?lm=1'],
-                    ['nr'=>23, 'naam'=>'Cedric Hatenboer',           'nat'=>'🇧🇪', 'foto'=>$tm.'682568-1768295695.png?lm=1'],
-                    ['nr'=>39, 'naam'=>'Jochem Ritmeester v/d Kamp', 'nat'=>'🇳🇱', 'foto'=>$tm.'865281-1768295609.JPG?lm=1'],
-                ],
-                'Aanvallers' => [
-                    ['nr'=>7,  'naam'=>'Soufiane Hetli',             'nat'=>'🇳🇱', 'foto'=>$tm.'779752-1768295005.JPG?lm=1'],
-                    ['nr'=>9,  'naam'=>'Jelani Seedorf',             'nat'=>'🇳🇱', 'foto'=>$tm.'1092384-1738073614.jpg?lm=1'],
-                    ['nr'=>11, 'naam'=>'Tyrese Noslin',              'nat'=>'🇨🇼', 'foto'=>$tm.'708005-1768295165.JPG?lm=1'],
-                    ['nr'=>19, 'naam'=>'Nökkvi Þeyr Þórisson',       'nat'=>'🇮🇸', 'foto'=>$tm.'381349-1753439100.jpg?lm=1'],
-                    ['nr'=>27, 'naam'=>'Patrick Brouwer',            'nat'=>'🇳🇱', 'foto'=>$tm.'593824-1768295464.JPG?lm=1'],
-                    ['nr'=>30, 'naam'=>'Kay Tejan',                  'nat'=>'🇳🇱', 'foto'=>$tm.'442885-1768295652.jpg?lm=1'],
-                    ['nr'=>37, 'naam'=>'Sem van Duijn',              'nat'=>'🇳🇱', 'foto'=>$tm.'926491-1744829339.jpg?lm=1'],
-                ],
-            ];
-
-            foreach($selectie as $positie => $spelers) {
-                echo '<section class="selectie-sectie">';
-                echo '<h2 class="positie-titel">' . $positie . '</h2>';
-                echo '<div class="selectie-grid">';
-                foreach($spelers as $s) {
-                    $captain = isset($s['captain']) ? '<span class="captain-badge">C</span>' : '';
-                    echo '
-                    <article class="speler-kaart-foto">
-                        <img class="speler-foto" src="' . $s['foto'] . '" alt="' . htmlspecialchars($s['naam']) . '" onerror="this.src=\'https://img.a.transfermarkt.technology/portrait/medium/default.jpg\'">
-                        <div class="speler-rugnr">' . $s['nr'] . '</div>
-                        <div class="speler-details">
-                            <div class="speler-naam-groot">' . htmlspecialchars($s['naam']) . ' ' . $captain . '</div>
-                            <div class="speler-meta">' . $s['nat'] . ' &nbsp; <span class="speler-pos">' . $positie . '</span></div>
+            <section class="resultaten-lijst">
+                <?php foreach($wedstrijden as $wed):
+                    $isThuisploeg = ($wed['thuisnaam'] == 'Telstar');
+                    $eigen = $isThuisploeg ? $wed['thuisscore'] : $wed['uitscore'];
+                    $tegen = $isThuisploeg ? $wed['uitscore'] : $wed['thuisscore'];
+                    if($eigen > $tegen) $uitslag = 'winst';
+                    elseif($eigen == $tegen) $uitslag = 'gelijk';
+                    else $uitslag = 'verlies';
+                ?>
+                <article class="wedstrijd-kaart <?=$uitslag?>">
+                    <div class="wedstrijd-datum">
+                        <span class="dag"><?=$wed['dag']?></span>
+                        <span class="datum"><?=sprintf('%02d-%02d-%04d', $wed['dd'], $wed['mm'], $wed['yyyy'])?></span>
+                    </div>
+                    <div class="wedstrijd-teams">
+                        <div class="team thuis <?=$isThuisploeg ? 'is-telstar' : ''?>">
+                            <img src="<?=getLogo($wed['thuisnaam'])?>" alt="<?=htmlspecialchars($wed['thuisnaam'])?>">
+                            <span><?=htmlspecialchars($wed['thuisnaam'])?></span>
                         </div>
-                    </article>';
-                }
-                echo '</div></section>';
-            }
-            ?>
+                        <div class="wedstrijd-score">
+                            <span class="score-getal"><?=$wed['thuisscore']?></span>
+                            <span class="score-streep">-</span>
+                            <span class="score-getal"><?=$wed['uitscore']?></span>
+                        </div>
+                        <div class="team uit <?=!$isThuisploeg ? 'is-telstar' : ''?>">
+                            <span><?=htmlspecialchars($wed['uitnaam'])?></span>
+                            <img src="<?=getLogo($wed['uitnaam'])?>" alt="<?=htmlspecialchars($wed['uitnaam'])?>">
+                        </div>
+                    </div>
+                    <div class="wedstrijd-badge <?=$uitslag?>">
+                        <?=($uitslag=='winst') ? 'W' : (($uitslag=='gelijk') ? 'G' : 'V')?>
+                    </div>
+                </article>
+                <?php endforeach; ?>
+            </section>
         </main>
 
         <footer>
